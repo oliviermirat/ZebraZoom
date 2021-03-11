fdcb3c0809cd0ffb9eb4bb010309df8116f0ec1a
olivierLenovo <miratoolivier@gmail.com>
2021-03-11 15:01:31 +0800

    adding option to convert videos from seq to avi

--- zebrazoom/__main__.py
+++ zebrazoom/__main__.py
@@ -15,43 +15,52 @@ if len(sys.argv) == 1:
     
 else:
 
-  if len(sys.argv) >= 5:
-
-    if sys.argv[1] == "getTailExtremityFirstFrame":
-      
-      pathToVideo = sys.argv[2]
-      videoName   = sys.argv[3]
-      videoExt    = sys.argv[4]
-      configFile  = sys.argv[5]
-      argv        = sys.argv
-      argv.pop(0)
-      from zebrazoom.getTailExtremityFirstFrame import getTailExtremityFirstFrame
-      if __name__ == '__main__':
-        __spec__ = "ModuleSpec(name='builtins', loader=<class '_frozen_importlib.BuiltinImporter'>)"
-        getTailExtremityFirstFrame(pathToVideo, videoName, videoExt, configFile, argv)
-        
-    elif sys.argv[1] == "recreateSuperStruct":
-      
-      pathToVideo = sys.argv[2]
-      videoName   = sys.argv[3]
-      videoExt    = sys.argv[4]
-      configFile  = sys.argv[5]
-      argv        = sys.argv
-      argv.pop(0)
-      from zebrazoom.recreateSuperStruct import recreateSuperStruct
-      if __name__ == '__main__':
-        __spec__ = "ModuleSpec(name='builtins', loader=<class '_frozen_importlib.BuiltinImporter'>)"
-        recreateSuperStruct(pathToVideo, videoName, videoExt, configFile, argv)
-        
-    else:
+  if sys.argv[1] == "getTailExtremityFirstFrame":
     
-      pathToVideo = sys.argv[1]
-      videoName   = sys.argv[2]
-      videoExt    = sys.argv[3]
-      configFile  = sys.argv[4]
-      argv        = sys.argv
-      from zebrazoom.mainZZ import mainZZ
-      if __name__ == '__main__':
-        __spec__ = "ModuleSpec(name='builtins', loader=<class '_frozen_importlib.BuiltinImporter'>)"
-        mainZZ(pathToVideo, videoName, videoExt, configFile, argv)
+    pathToVideo = sys.argv[2]
+    videoName   = sys.argv[3]
+    videoExt    = sys.argv[4]
+    configFile  = sys.argv[5]
+    argv        = sys.argv
+    argv.pop(0)
+    from zebrazoom.getTailExtremityFirstFrame import getTailExtremityFirstFrame
+    if __name__ == '__main__':
+      __spec__ = "ModuleSpec(name='builtins', loader=<class '_frozen_importlib.BuiltinImporter'>)"
+      getTailExtremityFirstFrame(pathToVideo, videoName, videoExt, configFile, argv)
+      
+  elif sys.argv[1] == "recreateSuperStruct":
+    
+    pathToVideo = sys.argv[2]
+    videoName   = sys.argv[3]
+    videoExt    = sys.argv[4]
+    configFile  = sys.argv[5]
+    argv        = sys.argv
+    argv.pop(0)
+    from zebrazoom.recreateSuperStruct import recreateSuperStruct
+    if __name__ == '__main__':
+      __spec__ = "ModuleSpec(name='builtins', loader=<class '_frozen_importlib.BuiltinImporter'>)"
+      recreateSuperStruct(pathToVideo, videoName, videoExt, configFile, argv)
+      
+  elif sys.argv[1] == "convertSeqToAvi":
+    
+    from zebrazoom.videoFormatConversion.seq_to_avi import sqb_convert_to_avi
+    path      = sys.argv[2]
+    videoName = sys.argv[3]
+    if len(sys.argv) == 5:
+      lastFrame = int(sys.argv[4])
+    else:
+      lastFrame = -1
+    sqb_convert_to_avi(path, videoName, lastFrame)
+
+  else:
+  
+    pathToVideo = sys.argv[1]
+    videoName   = sys.argv[2]
+    videoExt    = sys.argv[3]
+    configFile  = sys.argv[4]
+    argv        = sys.argv
+    from zebrazoom.mainZZ import mainZZ
+    if __name__ == '__main__':
+      __spec__ = "ModuleSpec(name='builtins', loader=<class '_frozen_importlib.BuiltinImporter'>)"
+      mainZZ(pathToVideo, videoName, videoExt, configFile, argv)
 
---
+++ zebrazoom/videoFormatConversion/seq_to_avi.py
@@ -0,0 +1,79 @@
+import struct
+import csv
+import mmap
+import datetime
+import array
+import logging
+import os
+import csv
+import glob
+import time
+import configparser
+import numpy as np
+import cv2
+from pathlib import Path
+
+def sqb_convert_to_avi(path, videoName, lastFrame):
+  """
+      Lecture du fichier binaire de séquence sqb
+      Les données sont représentées par la structure en C suivante :
+          typedef struct
+          {   
+              long offset;        // 4 bits -> + 4 bits vides car mémoire alignée
+              double TimeStamp;   // 8 bits
+              int binfile;        // 4 bits -> + 4 bits vides car mémoire alignée
+          } IMGDATA;
+  """
+  
+  seq_path  = os.path.join(path, videoName + '.seq')
+  path2 = Path(path)
+  avi_path  = os.path.join(path2.parent, videoName + '.avi')
+
+  cfg = configparser.ConfigParser()
+  cfg.read(seq_path)
+  
+  width = int(cfg.get('Sequence Settings', 'Width'))
+  height = int(cfg.get('Sequence Settings', 'Height'))
+  bpp = int(cfg.get('Sequence Settings', 'BytesPerPixel'))
+  num_images = cfg.get('Sequence Settings', 'Number of files')
+  bin_file = cfg.get('Sequence Settings', 'Bin File')
+  sqb_path = seq_path.replace('.seq', '.sqb')
+  
+  out = cv2.VideoWriter(avi_path, cv2.VideoWriter_fourcc('M','J','P','G'), 10, (width, height))
+  
+  pathstr = os.path.dirname(seq_path)
+  if lastFrame <= 0:
+    lastFrame = int(num_images)
+  
+  with open(sqb_path,'rb') as f :
+    for i in range(0, lastFrame):
+    
+      if (i % 500 == 0):
+        print("image " + str(i) + " out of " + str(lastFrame) + " in total")
+    
+      offset = struct.unpack('l', f.read(4))
+      padding = f.read(4)
+      timestamp = struct.unpack('d', f.read(8))
+      binfile = struct.unpack('i', f.read(4))
+      padding = f.read(4)
+      
+      bin_path = "%s\\%s%0.5d.bin" % (pathstr, bin_file, binfile[0])
+      
+      f_bin = open(bin_path, 'rb')
+      f_bin.seek(offset[0], os.SEEK_SET)
+      
+      bytes = f_bin.read(height*width*bpp)
+      
+      if bpp == 2:
+        buffer = np.frombuffer(bytes, dtype=np.uint16)
+      else:
+        buffer = np.frombuffer(bytes, dtype=np.uint8)
+      
+      nparr2 = buffer.reshape(height, width)
+      
+      nparr2 = cv2.cvtColor(nparr2, cv2.COLOR_GRAY2RGB) 
+      out.write(nparr2)
+      
+      f_bin.close()
+      
+  out.release()
