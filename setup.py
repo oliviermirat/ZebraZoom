60b8c81f0a74871ab71f566f5817c7b82f568120
olivierLenovo <miratoolivier@gmail.com>
2021-03-11 15:18:11 +0800

    fixing bug in previous version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 from distutils.core import setup
 setup(
   name = 'zebrazoom',
-  version = '0.49',
+  version = '0.50',
   license='AGPL-3.0',
   description = 'Track and analyze zebrafish and animal behavior',
   author = 'Olivier Mirat',
