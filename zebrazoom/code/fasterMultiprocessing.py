7945d3ae91e1d17416f29b4b6c5551b4da5a5efb
oliviermirat <miratoolivier@gmail.com>
2021-07-22 19:19:07 +0200

    fixing fasterMultiprocessing: last frames where causing bugs (because not checks that the frame extracted was valid

--- zebrazoom/code/fasterMultiprocessing.py
+++ zebrazoom/code/fasterMultiprocessing.py
@@ -49,46 +49,48 @@ def fasterMultiprocessing(videoPath, background, wellPositions, output, hyperpar
       
     ret, frame = cap.read()
     
-    for wellNumber in range(0,hyperparameters["nbWells"]):
+    if ret:
       
-      if hyperparameters["nbAnimalsPerWell"] == 1 and not(hyperparameters["forceBlobMethodForHeadTracking"]):
-        minPixelDiffForBackExtract = hyperparameters["minPixelDiffForBackExtract"]
-        xtop = wellPositions[wellNumber]['topLeftX']
-        ytop = wellPositions[wellNumber]['topLeftY']
-        lenX = wellPositions[wellNumber]['lengthX']
-        lenY = wellPositions[wellNumber]['lengthY']
-        back = background[ytop:ytop+lenY, xtop:xtop+lenX]
-        grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        curFrame = grey[ytop:ytop+lenY, xtop:xtop+lenX]
-        putToWhite = ( curFrame.astype('int32') >= (back.astype('int32') - minPixelDiffForBackExtract) )
-        curFrame[putToWhite] = 255
-        blur = cv2.GaussianBlur(curFrame, (hyperparameters["paramGaussianBlur"], hyperparameters["paramGaussianBlur"]),0)
-        headPositionFirstFrame = 0
-        thresh1 = 0
-        thresh2 = 0
-        gray    = 0
-      else:
-        [frame2, gray, thresh1, blur, thresh2, frame2, initialCurFrame, back] = getImages(hyperparameters, 0, videoPath, i, background, wellNumber, wellPositions, frame)
-        headPositionFirstFrame = 0
+      for wellNumber in range(0,hyperparameters["nbWells"]):
+        
+        if hyperparameters["nbAnimalsPerWell"] == 1 and not(hyperparameters["forceBlobMethodForHeadTracking"]):
+          minPixelDiffForBackExtract = hyperparameters["minPixelDiffForBackExtract"]
+          xtop = wellPositions[wellNumber]['topLeftX']
+          ytop = wellPositions[wellNumber]['topLeftY']
+          lenX = wellPositions[wellNumber]['lengthX']
+          lenY = wellPositions[wellNumber]['lengthY']
+          back = background[ytop:ytop+lenY, xtop:xtop+lenX]
+          grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+          curFrame = grey[ytop:ytop+lenY, xtop:xtop+lenX]
+          putToWhite = ( curFrame.astype('int32') >= (back.astype('int32') - minPixelDiffForBackExtract) )
+          curFrame[putToWhite] = 255
+          blur = cv2.GaussianBlur(curFrame, (hyperparameters["paramGaussianBlur"], hyperparameters["paramGaussianBlur"]),0)
+          headPositionFirstFrame = 0
+          thresh1 = 0
+          thresh2 = 0
+          gray    = 0
+        else:
+          [frame2, gray, thresh1, blur, thresh2, frame2, initialCurFrame, back] = getImages(hyperparameters, 0, videoPath, i, background, wellNumber, wellPositions, frame)
+          headPositionFirstFrame = 0
+        
+        # Head tracking and heading calculation
+        [trackingHeadingAllAnimalsList[wellNumber], trackingHeadTailAllAnimalsList[wellNumber], lastFirstTheta] = headTrackingHeadingCalculation(hyperparameters, firstFrame, i, blur, thresh1, thresh2, gray, hyperparameters["erodeSize"], frame_width, frame_height, trackingHeadingAllAnimalsList[wellNumber], trackingHeadTailAllAnimalsList[wellNumber], headPositionFirstFrame, wellPositions[wellNumber]["lengthX"])
+        
+        # Tail tracking for frame i
+        if hyperparameters["trackTail"] == 1 :
+          threshForBlackFrames = 0
+          thetaDiffAccept = 1.2
+          lastFirstTheta = 0
+          maxDepth = 0
+          tailTipFirstFrame = []
+          for animalId in range(0, hyperparameters["nbAnimalsPerWell"]):
+            [trackingHeadTailAllAnimalsList[wellNumber], trackingHeadingAllAnimalsList[wellNumber]] = tailTracking(animalId, i, firstFrame, videoPath, frame, hyperparameters, thresh1, nbTailPoints, threshForBlackFrames, thetaDiffAccept, trackingHeadTailAllAnimalsList[wellNumber], trackingHeadingAllAnimalsList[wellNumber], lastFirstTheta, maxDepth, tailTipFirstFrame, initialCurFrame.copy(), back)
+        
+        # Eye tracking for frame i
+        if hyperparameters["eyeTracking"]:
+          trackingEyesAllAnimalsList[wellNumber] = eyeTracking(animalId, i, firstFrame, frame, hyperparameters, thresh1, trackingHeadingAllAnimalsList[wellNumber], trackingHeadTailAllAnimalsList[wellNumber], trackingEyesAllAnimalsList[wellNumber])
       
-      # Head tracking and heading calculation
-      [trackingHeadingAllAnimalsList[wellNumber], trackingHeadTailAllAnimalsList[wellNumber], lastFirstTheta] = headTrackingHeadingCalculation(hyperparameters, firstFrame, i, blur, thresh1, thresh2, gray, hyperparameters["erodeSize"], frame_width, frame_height, trackingHeadingAllAnimalsList[wellNumber], trackingHeadTailAllAnimalsList[wellNumber], headPositionFirstFrame, wellPositions[wellNumber]["lengthX"])
-      
-      # Tail tracking for frame i
-      if hyperparameters["trackTail"] == 1 :
-        threshForBlackFrames = 0
-        thetaDiffAccept = 1.2
-        lastFirstTheta = 0
-        maxDepth = 0
-        tailTipFirstFrame = []
-        for animalId in range(0, hyperparameters["nbAnimalsPerWell"]):
-          [trackingHeadTailAllAnimalsList[wellNumber], trackingHeadingAllAnimalsList[wellNumber]] = tailTracking(animalId, i, firstFrame, videoPath, frame, hyperparameters, thresh1, nbTailPoints, threshForBlackFrames, thetaDiffAccept, trackingHeadTailAllAnimalsList[wellNumber], trackingHeadingAllAnimalsList[wellNumber], lastFirstTheta, maxDepth, tailTipFirstFrame, initialCurFrame.copy(), back)
-      
-      # Eye tracking for frame i
-      if hyperparameters["eyeTracking"]:
-        trackingEyesAllAnimalsList[wellNumber] = eyeTracking(animalId, i, firstFrame, frame, hyperparameters, thresh1, trackingHeadingAllAnimalsList[wellNumber], trackingHeadTailAllAnimalsList[wellNumber], trackingEyesAllAnimalsList[wellNumber])
-    
-    i = i + 1
+      i = i + 1
     
   for wellNumber in range(0,hyperparameters["nbWells"]):
     [trackingHeadingAllAnimalsList[wellNumber], trackingHeadTailAllAnimalsList[wellNumber], trackingEyesAllAnimals] = postProcessMultipleTrajectories(trackingHeadingAllAnimalsList[wellNumber], trackingHeadTailAllAnimalsList[wellNumber], [], hyperparameters)  
