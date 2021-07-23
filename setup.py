a58f9641e6a2eecbae3ac99153aecbf09e81891c
oliviermirat <miratoolivier@gmail.com>
2021-07-23 10:19:55 +0200

    improving createSuperStructure in order to not calculate tailangle when the tail is not tracked

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 from distutils.core import setup
 setup(
   name = 'zebrazoom',
-  version = '1.02',
+  version = '1.03',
   license='AGPL-3.0',
   description = 'Track and analyze zebrafish and animal behavior',
   author = 'Olivier Mirat',
--- zebrazoom/code/createSuperStruct.py
+++ zebrazoom/code/createSuperStruct.py
@@ -98,135 +98,140 @@ def createSuperStruct(dataPerWell, wellPositions, hyperparameters):
     tab  = [[] for idAnimal in range(0, hyperparameters["nbAnimalsPerWell"])]
     
     for i in range(0, nbMouv):
+    
       item = j[i]
       
-      angle_raw = item["TailAngle_Raw"]
-      
-      if len(angle_raw) > 10:
-      
-        rolling_window = hyperparameters["tailAngleMedianFilter"]
-        if rolling_window > 0:
-          shift = int(-rolling_window / 2)
-          angle_median = np.array(pd.Series(angle_raw).rolling(rolling_window).median())
-          angle_median = np.roll(angle_median, shift)
-          for ii in range(0, rolling_window):
-            angle_median[ii] = angle_raw[ii]
-          for ii in range(len(angle_median)-rolling_window,len(angle_median)):
-            angle_median[ii] = angle_raw[ii]
+      if hyperparameters["trackTail"]:
+        
+        angle_raw = item["TailAngle_Raw"]
+        
+        if len(angle_raw) > 10:
+        
+          rolling_window = hyperparameters["tailAngleMedianFilter"]
+          if rolling_window > 0:
+            shift = int(-rolling_window / 2)
+            angle_median = np.array(pd.Series(angle_raw).rolling(rolling_window).median())
+            angle_median = np.roll(angle_median, shift)
+            for ii in range(0, rolling_window):
+              angle_median[ii] = angle_raw[ii]
+            for ii in range(len(angle_median)-rolling_window,len(angle_median)):
+              angle_median[ii] = angle_raw[ii]
+          else:
+            angle_median = angle_raw
+        
+          x = np.linspace(0, 1, len(angle_median))
+          
+          # if True: # Original method
+          s = UnivariateSpline(x, angle_median, s=tailAngleSmoothingFactor) # This might be non-terminating sometimes, need to investigate this in the future
+          TailAngle_smoothed = s(x) 
+          
+          item['TailAngle_smoothed'] = TailAngle_smoothed.tolist()
         else:
-          angle_median = angle_raw
-      
-        x = np.linspace(0, 1, len(angle_median))
+          TailAngle_smoothed = np.array(angle_raw)
+          item['TailAngle_smoothed'] = angle_raw
         
-        # if True: # Original method
-        s = UnivariateSpline(x, angle_median, s=tailAngleSmoothingFactor)
-        TailAngle_smoothed = s(x) 
+        # if hyperparameters["calculateAllTailAngles"]:
+          # [tailangles_arr, tailangles_arr_smoothed] = calculateAllTailAngles(item, tailAngleSmoothingFactor, hyperparameters)
+          # item["allTailAngles"]         = tailangles_arr.tolist()
+          # item["allTailAnglesSmoothed"] = tailangles_arr_smoothed.tolist()
         
-        item['TailAngle_smoothed'] = TailAngle_smoothed.tolist()
-      else:
-        TailAngle_smoothed = np.array(angle_raw)
-        item['TailAngle_smoothed'] = angle_raw
-      
-      # if hyperparameters["calculateAllTailAngles"]:
-        # [tailangles_arr, tailangles_arr_smoothed] = calculateAllTailAngles(item, tailAngleSmoothingFactor, hyperparameters)
-        # item["allTailAngles"]         = tailangles_arr.tolist()
-        # item["allTailAnglesSmoothed"] = tailangles_arr_smoothed.tolist()
-      
-      if hyperparameters['extractAdvanceZebraParameters']:
-      
-        maxDiffPeakToPeak = 0
-        maxAngle = max([TailAngle_smoothed[l] for l in range(0, len(TailAngle_smoothed))])
-        minAngle = min([TailAngle_smoothed[l] for l in range(0, len(TailAngle_smoothed))])
-        maxDiffPeakToPeak = maxAngle - minAngle
+        if hyperparameters['extractAdvanceZebraParameters']:
         
-        minProminenceForBendsDetect = hyperparameters["minProminenceForBendsDetect"]
-        if minProminenceForBendsDetect == -1:
-          minProminenceForBendsDetect = maxDiffPeakToPeak / 10
-        
-        maxpeaks, properties = find_peaks(TailAngle_smoothed, prominence=minProminenceForBendsDetect, width=hyperparameters["windowForLocalBendMinMaxFind"])
-        minpeaks, properties = find_peaks(-TailAngle_smoothed, prominence=minProminenceForBendsDetect, width=hyperparameters["windowForLocalBendMinMaxFind"])
-        
-        if (len(minpeaks) + len(maxpeaks) >= hyperparameters["minNbPeaksForBoutDetect"] ):
-        
-          ind = 1
-          minDiffBetweenSubsequentBendAmp = hyperparameters["minDiffBetweenSubsequentBendAmp"]
+          maxDiffPeakToPeak = 0
+          maxAngle = max([TailAngle_smoothed[l] for l in range(0, len(TailAngle_smoothed))])
+          minAngle = min([TailAngle_smoothed[l] for l in range(0, len(TailAngle_smoothed))])
+          maxDiffPeakToPeak = maxAngle - minAngle
           
-          Bend_Timing = []
-          lastTailValue = 100000
+          minProminenceForBendsDetect = hyperparameters["minProminenceForBendsDetect"]
+          if minProminenceForBendsDetect == -1:
+            minProminenceForBendsDetect = maxDiffPeakToPeak / 10
+          
+          maxpeaks, properties = find_peaks(TailAngle_smoothed, prominence=minProminenceForBendsDetect, width=hyperparameters["windowForLocalBendMinMaxFind"])
+          minpeaks, properties = find_peaks(-TailAngle_smoothed, prominence=minProminenceForBendsDetect, width=hyperparameters["windowForLocalBendMinMaxFind"])
+          
+          if (len(minpeaks) + len(maxpeaks) >= hyperparameters["minNbPeaksForBoutDetect"] ):
+          
+            ind = 1
+            minDiffBetweenSubsequentBendAmp = hyperparameters["minDiffBetweenSubsequentBendAmp"]
+            
+            Bend_Timing = []
+            lastTailValue = 100000
 
-          for i in range(item['BoutStart'],item['BoutEnd']+1):
-          
-            minOrMax = IsMinOrMax(maxpeaks, minpeaks, i - item['BoutStart'] )
+            for i in range(item['BoutStart'],item['BoutEnd']+1):
             
-            if minOrMax:
-              if (abs(lastTailValue - TailAngle_smoothed[i - item['BoutStart'] ]) > minDiffBetweenSubsequentBendAmp):
-                Bend_Timing.append(i - item['BoutStart'] + 1) 
-                lastTailValue = TailAngle_smoothed[i- item['BoutStart'] ]
-            
-            if (len(Bend_Timing) == 1) and (abs(lastTailValue) < hyperparameters["minFirstBendValue"]):
-              Bend_Timing = []
+              minOrMax = IsMinOrMax(maxpeaks, minpeaks, i - item['BoutStart'] )
               
-          if len(Bend_Timing) > 3:
-            Bend_Timing2 = []
-            if hyperparameters["doubleCheckBendMinMaxStatus"]:
-              # Checking first bend
-              bendId = 0
-              ind2 = Bend_Timing[bendId]
-              ind3 = Bend_Timing[bendId+1]
-              cond1 = (TailAngle_smoothed[0] < TailAngle_smoothed[ind2-1]) and (TailAngle_smoothed[ind3-1] < TailAngle_smoothed[ind2-1])
-              cond2 = (TailAngle_smoothed[0] > TailAngle_smoothed[ind2-1]) and (TailAngle_smoothed[ind3-1] > TailAngle_smoothed[ind2-1])
-              if cond1 or cond2:
-                Bend_Timing2.append(ind2)
-              # Checking bends in the middle
-              for bendId in range(1, len(Bend_Timing)-1):
-                ind1 = Bend_Timing[bendId-1]
+              if minOrMax:
+                if (abs(lastTailValue - TailAngle_smoothed[i - item['BoutStart'] ]) > minDiffBetweenSubsequentBendAmp):
+                  Bend_Timing.append(i - item['BoutStart'] + 1) 
+                  lastTailValue = TailAngle_smoothed[i- item['BoutStart'] ]
+              
+              if (len(Bend_Timing) == 1) and (abs(lastTailValue) < hyperparameters["minFirstBendValue"]):
+                Bend_Timing = []
+                
+            if len(Bend_Timing) > 3:
+              Bend_Timing2 = []
+              if hyperparameters["doubleCheckBendMinMaxStatus"]:
+                # Checking first bend
+                bendId = 0
                 ind2 = Bend_Timing[bendId]
                 ind3 = Bend_Timing[bendId+1]
-                cond1 = (TailAngle_smoothed[ind1-1] < TailAngle_smoothed[ind2-1]) and (TailAngle_smoothed[ind3-1] < TailAngle_smoothed[ind2-1])
-                cond2 = (TailAngle_smoothed[ind1-1] > TailAngle_smoothed[ind2-1]) and (TailAngle_smoothed[ind3-1] > TailAngle_smoothed[ind2-1])
+                cond1 = (TailAngle_smoothed[0] < TailAngle_smoothed[ind2-1]) and (TailAngle_smoothed[ind3-1] < TailAngle_smoothed[ind2-1])
+                cond2 = (TailAngle_smoothed[0] > TailAngle_smoothed[ind2-1]) and (TailAngle_smoothed[ind3-1] > TailAngle_smoothed[ind2-1])
                 if cond1 or cond2:
                   Bend_Timing2.append(ind2)
-              # Checking last bend
-              bendId = len(Bend_Timing)-1
-              ind1 = Bend_Timing[bendId-1]
-              ind2 = Bend_Timing[bendId]
-              n = len(TailAngle_smoothed)-1
-              cond1 = (TailAngle_smoothed[ind1-1] < TailAngle_smoothed[ind2-1]) and (TailAngle_smoothed[n] < TailAngle_smoothed[ind2-1])
-              cond2 = (TailAngle_smoothed[ind1-1] > TailAngle_smoothed[ind2-1]) and (TailAngle_smoothed[n] > TailAngle_smoothed[ind2-1])
-              if cond1 or cond2:
-                Bend_Timing2.append(ind2)
-              Bend_Timing = Bend_Timing2
-          
-          if hyperparameters["removeFirstSmallBend"] and len(Bend_Timing) > 1:
-            ind1 = Bend_Timing[0]
-            ind2 = Bend_Timing[1]
-            if abs(TailAngle_smoothed[ind1-1]) < abs(TailAngle_smoothed[ind2-1]) / hyperparameters["removeFirstSmallBend"]:
-              Bend_Timing.pop(0)
-          
-          item['Bend_Timing'] = Bend_Timing
-          
-          Bend_TimingAbsolute = [val + item['BoutStart'] for val in Bend_Timing]
-          
-          item['Bend_TimingAbsolute'] = Bend_TimingAbsolute
-          
-          Bend_Amplitude = [TailAngle_smoothed[val-1] for val in Bend_Timing]
-          item['Bend_Amplitude'] = Bend_Amplitude
-          
-          tab[item["AnimalNumber"]].append(item)
+                # Checking bends in the middle
+                for bendId in range(1, len(Bend_Timing)-1):
+                  ind1 = Bend_Timing[bendId-1]
+                  ind2 = Bend_Timing[bendId]
+                  ind3 = Bend_Timing[bendId+1]
+                  cond1 = (TailAngle_smoothed[ind1-1] < TailAngle_smoothed[ind2-1]) and (TailAngle_smoothed[ind3-1] < TailAngle_smoothed[ind2-1])
+                  cond2 = (TailAngle_smoothed[ind1-1] > TailAngle_smoothed[ind2-1]) and (TailAngle_smoothed[ind3-1] > TailAngle_smoothed[ind2-1])
+                  if cond1 or cond2:
+                    Bend_Timing2.append(ind2)
+                # Checking last bend
+                bendId = len(Bend_Timing)-1
+                ind1 = Bend_Timing[bendId-1]
+                ind2 = Bend_Timing[bendId]
+                n = len(TailAngle_smoothed)-1
+                cond1 = (TailAngle_smoothed[ind1-1] < TailAngle_smoothed[ind2-1]) and (TailAngle_smoothed[n] < TailAngle_smoothed[ind2-1])
+                cond2 = (TailAngle_smoothed[ind1-1] > TailAngle_smoothed[ind2-1]) and (TailAngle_smoothed[n] > TailAngle_smoothed[ind2-1])
+                if cond1 or cond2:
+                  Bend_Timing2.append(ind2)
+                Bend_Timing = Bend_Timing2
+            
+            if hyperparameters["removeFirstSmallBend"] and len(Bend_Timing) > 1:
+              ind1 = Bend_Timing[0]
+              ind2 = Bend_Timing[1]
+              if abs(TailAngle_smoothed[ind1-1]) < abs(TailAngle_smoothed[ind2-1]) / hyperparameters["removeFirstSmallBend"]:
+                Bend_Timing.pop(0)
+            
+            item['Bend_Timing'] = Bend_Timing
+            
+            Bend_TimingAbsolute = [val + item['BoutStart'] for val in Bend_Timing]
+            
+            item['Bend_TimingAbsolute'] = Bend_TimingAbsolute
+            
+            Bend_Amplitude = [TailAngle_smoothed[val-1] for val in Bend_Timing]
+            item['Bend_Amplitude'] = Bend_Amplitude
+            
+            tab[item["AnimalNumber"]].append(item)
+            
+          else:
+            item['Bend_Timing'] = []
+            item['Bend_TimingAbsolute'] = []
+            item['Bend_Amplitude'] = []
+            item['TailAngle_raw'] = []
+            item['TailAngle_smoothed'] = []
+            tab[item["AnimalNumber"]].append(item)
           
         else:
-          item['Bend_Timing'] = []
-          item['Bend_TimingAbsolute'] = []
-          item['Bend_Amplitude'] = []
           item['TailAngle_raw'] = []
           item['TailAngle_smoothed'] = []
           tab[item["AnimalNumber"]].append(item)
-        
       else:
-        item['TailAngle_raw'] = []
-        item['TailAngle_smoothed'] = []
         tab[item["AnimalNumber"]].append(item)
-    
+      
     wellPoissMouv.append(tab)
   
   videoDataResults['wellPoissMouv'] = wellPoissMouv
