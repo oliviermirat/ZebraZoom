9135a55ea28fe95aa8a8864c181e6e4054e8e72d
Olivier Mirat <miratoolivier@gmail.com>
2022-04-01 19:46:14 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 from distutils.core import setup
 setup(
   name = 'zebrazoom',
-  version = '1.32.35',
+  version = '1.32.36',
   license='AGPL-3.0',
   description = 'Track and analyze zebrafish and animal behavior',
   author = 'Olivier Mirat',
