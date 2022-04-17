97d37cfef635d7e6e6d9f14508ff30e0fbcf5f3e
oliviermirat <miratoolivier@gmail.com>
2022-04-17 09:47:02 +0000

    fixing situations where firstFrame is not equal to 1

--- zebrazoom/dataAnalysis/createCustomDataStructure/createDistanceBetweenFramesExcelFile.py
+++ zebrazoom/dataAnalysis/createCustomDataStructure/createDistanceBetweenFramesExcelFile.py
@@ -29,23 +29,31 @@ def createDistanceBetweenFramesExcelFile(ZZoutputFolder, paramList):
     supstruct = json.load(video)
     
     numberOfWells = len(supstruct['wellPoissMouv'])
+    firstFrame = 0
+    lastFrame  = 0
     if ('lastFrame' in supstruct) and ('firstFrame' in supstruct):
       totalNumberOfFrames = supstruct['lastFrame'] - supstruct['firstFrame'] + 1
+      firstFrame = int(supstruct['firstFrame'])
+      lastFrame  = int(supstruct['lastFrame'])
     else:
       raise NameError('firstFrame and lastFrame are not in the superstructure results file!')
     
     print("totalNumberOfFrames: ", totalNumberOfFrames)
 
-    data = pd.DataFrame(data=[[wellNum, frameNum, frameNum/fps, (frameNum/fps)/60, float('nan'), float('nan'), 0] for wellNum in range(0, numberOfWells) for frameNum in range(0, totalNumberOfFrames)], columns=['wellId', 'frameNumber', 'second', 'minute', 'xPosition', 'yPosition', 'instantaneousDistance'])
+    data = pd.DataFrame(data=[[wellNum, frameNum, frameNum/fps, (frameNum/fps)/60, float('nan'), float('nan'), 0] for wellNum in range(0, numberOfWells) for frameNum in range(firstFrame, lastFrame)], columns=['wellId', 'frameNumber', 'second', 'minute', 'xPosition', 'yPosition', 'instantaneousDistance'])
     
     for numWell in range(0, len(supstruct['wellPoissMouv'])):  
       for numAnimal in range(0, len(supstruct['wellPoissMouv'][numWell])):
         for numBout in range(0, len(supstruct['wellPoissMouv'][numWell][numAnimal])):
           bout = supstruct['wellPoissMouv'][numWell][numAnimal][numBout]
-          if bout["BoutEnd"] < totalNumberOfFrames:
-            print("numWell:", numWell, "; numBout:", numBout, "; boutStart:", bout["BoutStart"])
+          # if bout["BoutEnd"] < totalNumberOfFrames:
+          print("numWell:", numWell, "; numBout:", numBout, "; boutStart:", bout["BoutStart"])
+          try:
             data.loc[np.logical_and(np.logical_and(data['wellId'] == numWell, data['frameNumber'] >= bout["BoutStart"]), data['frameNumber'] <= bout["BoutEnd"]), 'xPosition'] = bout['HeadX']
             data.loc[np.logical_and(np.logical_and(data['wellId'] == numWell, data['frameNumber'] >= bout["BoutStart"]), data['frameNumber'] <= bout["BoutEnd"]), 'yPosition'] = bout['HeadY']
+          except:
+            data.loc[np.logical_and(np.logical_and(data['wellId'] == numWell, data['frameNumber'] >= bout["BoutStart"]), data['frameNumber'] <= bout["BoutEnd"]), 'xPosition'] = bout['HeadX'][0:len(data.loc[np.logical_and(np.logical_and(data['wellId'] == numWell, data['frameNumber'] >= bout["BoutStart"]), data['frameNumber'] <= bout["BoutEnd"]), 'xPosition'])]
+            data.loc[np.logical_and(np.logical_and(data['wellId'] == numWell, data['frameNumber'] >= bout["BoutStart"]), data['frameNumber'] <= bout["BoutEnd"]), 'yPosition'] = bout['HeadY'][0:len(data.loc[np.logical_and(np.logical_and(data['wellId'] == numWell, data['frameNumber'] >= bout["BoutStart"]), data['frameNumber'] <= bout["BoutEnd"]), 'yPosition'])]
   
   # Calculating the "instantaneousDistance"
   previousNumWell = 0
@@ -69,7 +77,7 @@ def createDistanceBetweenFramesExcelFile(ZZoutputFolder, paramList):
   for wellId in range(0, numberOfWells):
     wellData = data.loc[data['wellId'] == wellId]
     if type(finalDataArray) == int:
-      finalDataArray = np.concatenate((np.transpose(np.array([[i for i in range(0, len(wellData['instantaneousDistance']))]])), np.transpose(np.array([[i/fps for i in range(0, len(wellData['instantaneousDistance']))]])), np.transpose(np.array([wellData['instantaneousDistance']]))), axis=1)
+      finalDataArray = np.concatenate((np.transpose(np.array([[i for i in range(firstFrame, lastFrame)]])), np.transpose(np.array([[i/fps for i in range(firstFrame, lastFrame)]])), np.transpose(np.array([wellData['instantaneousDistance']]))), axis=1)
     else:
       finalDataArray = np.concatenate((finalDataArray, np.transpose(np.array([wellData['instantaneousDistance']]))), axis=1)
   
