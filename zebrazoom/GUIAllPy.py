ee73800787f7d0b910d477d9eff74cef7382cda9
oliviermirat <miratoolivier@gmail.com>
2022-05-11 14:09:21 +0200

    testing new release system

--- zebrazoom/GUIAllPy.py
+++ zebrazoom/GUIAllPy.py
@@ -85,7 +85,7 @@ class PlainApplication(QApplication):
 class ZebraZoomApp(PlainApplication):
     def __init__(self, args):
         super().__init__(args)
-        self.version = '1.32.49'
+        self.version = '1.32.50'
 
         self.homeDirectory = paths.getRootDataFolder()
 
