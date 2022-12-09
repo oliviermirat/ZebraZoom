425c1c5fa19d7c077545614b9e3b673d548b1978
oliviermirat <miratoolivier@gmail.com>
2022-12-09 21:39:01 +0800

    adding boutNumber to previously create10anglePointsAllFramesCombinedDataFrame uploaded script

--- readAndAnalyzeZZoutputWithPython/create10anglePointsAllFramesCombinedDataFrame.py
+++ readAndAnalyzeZZoutputWithPython/create10anglePointsAllFramesCombinedDataFrame.py
@@ -7,6 +7,7 @@ pathToResultFile = 'results_headEmbeddedZebrafishLarva.txt'
 TailX_VideoReferential_All = []
 TailY_VideoReferential_All = []
 frameNumber_All = []
+numBout_All = []
 
 with open(pathToResultFile) as video:
   supstruct = json.load(video)
@@ -17,12 +18,14 @@ with open(pathToResultFile) as video:
         TailX_VideoReferential_All = TailX_VideoReferential_All + bout['TailX_VideoReferential']
         TailY_VideoReferential_All = TailY_VideoReferential_All + bout['TailY_VideoReferential']
         frameNumber_All = frameNumber_All + [i for i in range(bout['BoutStart'], bout['BoutEnd'] + 1)]
+        numBout_All     = numBout_All + [numBout for i in range(bout['BoutStart'], bout['BoutEnd'] + 1)]
 
 TailX_VideoReferential_All = np.array(TailX_VideoReferential_All)
 TailY_VideoReferential_All = np.array(TailY_VideoReferential_All)
 frameNumber_All            = np.array([frameNumber_All]).transpose()
+numBout_All                = np.array([numBout_All]).transpose()
 
-df = pd.DataFrame(np.concatenate((frameNumber_All, np.concatenate((TailX_VideoReferential_All, TailY_VideoReferential_All), axis=1)), axis=1), columns=['frameNumber'] + ['TailPosX' + str(i) for i in range(1, 11)] + ['TailPosY' + str(i) for i in range(1, 11)])
+df = pd.DataFrame(np.concatenate((frameNumber_All, np.concatenate((numBout_All, np.concatenate((TailX_VideoReferential_All, TailY_VideoReferential_All), axis=1)), axis=1)), axis=1), columns=['frameNumber', 'boutNumber'] + ['TailPosX' + str(i) for i in range(1, 11)] + ['TailPosY' + str(i) for i in range(1, 11)])
 
 df.to_excel('10anglePointsForAllBoutsCombined.xlsx')
 df.to_pickle('10anglePointsForAllBoutsCombined.pkl')
