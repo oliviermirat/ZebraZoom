dff03e9773b77e0331ebfc878b1ab0e7d7654b52
anthepro <ante.sikic@mail.ru>
2023-05-24 09:52:53 +0200

    Add the missing file.

---
+++ zebrazoom/dataAPI/_openResultsFile.py
@@ -0,0 +1,23 @@
+import contextlib
+import os
+
+import h5py
+
+from zebrazoom.code.paths import getDefaultZZoutputFolder
+
+
+def _findResultsFile(videoName):
+  ZZoutputPath = getDefaultZZoutputFolder()
+  resultsPath = os.path.join(ZZoutputPath, f'{videoName}.h5')
+  if os.path.exists(resultsPath):  # exact match
+    return resultsPath
+  path = next(reversed(sorted(name for name in os.listdir(ZZoutputPath)
+                              if os.path.splitext(name)[0][:-20] == videoName)), None)
+  if path is None:
+    raise ValueError(f'video {videoName} not found in the default ZZoutput folder ({ZZoutputPath})')
+  return os.path.join(ZZoutputPath, path)
+
+@contextlib.contextmanager
+def openResultsFile(videoName, mode):
+  with h5py.File(_findResultsFile(videoName), mode) as results:
+    yield results
