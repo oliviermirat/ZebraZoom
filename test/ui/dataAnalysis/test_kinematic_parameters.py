e4a6680e3cabc2fa050a3b7fc9cd0c255b874293
anthepro <ante.sikic@mail.ru>
2023-05-26 18:23:25 +0200

    Update long running tests to reflect the new parameter addition.

--- test/ui/dataAnalysis/test_kinematic_parameters.py
+++ test/ui/dataAnalysis/test_kinematic_parameters.py
@@ -473,8 +473,9 @@ def _test_basic_check_results(expectedResults=_EXPECTED_RESULTS):
   assert not os.path.exists(os.path.join(dataFolder, 'Experiment 2.mat'))
 
   for folder in ('allBoutsMixed', 'medianPerWellFirst'):
-    assert set(os.listdir(os.path.join(outputFolder, folder))) == {'globalParametersInsideCategories_1.png', 'globalParametersInsideCategories.xlsx', 'globalParametersInsideCategories.csv', 'noMeanAndOutliersPlotted'}
-    assert set(os.listdir(os.path.join(outputFolder, folder, 'noMeanAndOutliersPlotted'))) == {'globalParametersInsideCategories_1.png'}
+    chartCount = 2 if folder == 'allBoutsMixed' else 3
+    assert set(os.listdir(os.path.join(outputFolder, folder))) == {'globalParametersInsideCategories_%d.png' % idx for idx in range(1, chartCount)} | {'globalParametersInsideCategories.xlsx', 'globalParametersInsideCategories.csv', 'noMeanAndOutliersPlotted'}
+    assert set(os.listdir(os.path.join(outputFolder, folder, 'noMeanAndOutliersPlotted'))) == {'globalParametersInsideCategories_%d.png' % idx for idx in range(1, chartCount)}
 
 
 @pytest.mark.long
@@ -689,8 +690,9 @@ def _test_frames_for_distance_calculation_check_results():
     assert_series_equal(expectedResultsAll[col], dataframe[col])
 
   for folder in ('allBoutsMixed', 'medianPerWellFirst'):
-    assert set(os.listdir(os.path.join(outputFolder, folder))) == {'globalParametersInsideCategories_1.png', 'globalParametersInsideCategories.xlsx', 'globalParametersInsideCategories.csv', 'noMeanAndOutliersPlotted'}
-    assert set(os.listdir(os.path.join(outputFolder, folder, 'noMeanAndOutliersPlotted'))) == {'globalParametersInsideCategories_1.png'}
+    chartCount = 2 if folder == 'allBoutsMixed' else 3
+    assert set(os.listdir(os.path.join(outputFolder, folder))) == {'globalParametersInsideCategories_%d.png' % idx for idx in range(1, chartCount)} | {'globalParametersInsideCategories.xlsx', 'globalParametersInsideCategories.csv', 'noMeanAndOutliersPlotted'}
+    assert set(os.listdir(os.path.join(outputFolder, folder, 'noMeanAndOutliersPlotted'))) == {'globalParametersInsideCategories_%d.png' % idx for idx in range(1, chartCount)}
 
 
 @pytest.mark.long
