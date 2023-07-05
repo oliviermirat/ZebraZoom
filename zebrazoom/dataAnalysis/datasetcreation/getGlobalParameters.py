fixing small bug in kinematic parameters calculation in case where a bout is only of one frame long

zebrazoom/dataAnalysis/datasetcreation/getGlobalParameters.py
@@ -83,7 +83,10 @@ def getGlobalParameters(curbout, fps, pixelSize, frameStepForDistanceCalculation
       instantaneousSpeed = []
       for j in range(0, len(posX)-1):
         instantaneousSpeed.append(math.sqrt((posX[j+1] - posX[j])**2 + (posY[j+1] - posY[j])**2) * pixelSize * fps)
-      listOfParametersCalculated.append(np.max(instantaneousSpeed))
+      if len(instantaneousSpeed):
+        listOfParametersCalculated.append(np.max(instantaneousSpeed))
+      else:
+        listOfParametersCalculated.append(0)
     
     
     elif parameterToCalculate == 'percentOfMovingFramesBasedOnDistance':