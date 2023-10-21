ec68f542b24a49ca3b6caedc9f5782137f32ea72
oliviermirat <miratoolivier@gmail.com>
2023-10-21 17:42:25 +0800

    adding script to remove outliers frames and plot FFT analysis

---
+++ readAndAnalyzeZZoutputWithPython/removeOutliersFramesAndPlotFFTExample.py
@@ -0,0 +1,110 @@
+import zebrazoom.dataAPI as dataAPI
+from scipy.fft import fft, fftfreq
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+### Parameters to change
+
+usingHdf5format = False
+videoFPS = 160
+
+if usingHdf5format:
+  videoName = "C:/Users/mirat/Desktop/openZZ/ZebraZoom/zebrazoom/ZZoutput/23.05.19.ao-07-f-1-2-long1_2023_10_02-11_47_42.h5"
+  keepOnlyBouts = True
+  removeSuspiciousFrames = True
+  tailLenghtMaxThresh = 0.1
+  tailAngleDiffLowerThresh = 0.1
+  tailAngleDiffUpperThresh = 0.3
+else:
+  excelData = ["../zebrazoom/ZZoutput/23.05.19.ao-07-f-1-2-long1_2023_10_01-08_55_57/allData_23.05.19.ao-07-f-1-2-long1_wellNumber0_animal0.csv", "../zebrazoom/ZZoutput/23.05.19.ao-07-f-1-2-long1_2023_10_01-08_55_57/allData_23.05.19.ao-07-f-1-2-long1_wellNumber0_animal1.csv"]
+  keepOnlyBouts = True
+  removeSuspiciousFrames = True
+  tailLenghtMaxThresh = 0.1
+  tailAngleDiffLowerThresh = 0
+  tailAngleDiffUpperThresh = 0.4
+
+####
+
+def getMinMaxDiffWithinSegment(nparray, startSegment, segmentLength):
+  return max(nparray[startSegment:startSegment+segmentLength]) - min(nparray[startSegment:startSegment+segmentLength])
+
+if usingHdf5format:
+  videoPixelSize = 0.01
+  dataAPI.setFPSandPixelSize(videoName, videoFPS, videoPixelSize)
+  startTimeInSeconds = 0.00625
+  endTimeInSeconds   = 600
+
+numWell = 0
+
+for numAnimal in [0, 1]:
+
+  if usingHdf5format:
+    TailAngle0  = dataAPI.getDataPerTimeInterval(videoName, numWell, numAnimal, startTimeInSeconds, endTimeInSeconds, "TailAngle")
+    TailLength0 = dataAPI.getDataPerTimeInterval(videoName, numWell, numAnimal, startTimeInSeconds, endTimeInSeconds, "TailLength")
+    boutTimings = dataAPI.listAllBouts(videoName, numWell, numAnimal)
+  else:
+    pandasData = pd.read_csv(excelData[numAnimal])
+    TailAngle0  = pandasData["tailAngle"].to_numpy()
+    TailLength0 = pandasData["TailLength"].to_numpy()
+
+  FrameNumber0 = np.array([i for i in range(len(TailAngle0))])
+  totalLength0 = 0
+  if usingHdf5format:
+    for timing in boutTimings:
+      totalLength0 += timing[1] - timing[0]
+    print("percentage of time spent swimming:", totalLength0 / (10 * 60 * videoFPS))
+  if keepOnlyBouts:
+    if usingHdf5format:
+      TailAngle0b   = np.array([])
+      TailLength0b  = np.array([])
+      FrameNumber0b = np.array([])
+      for timing in boutTimings:
+        TailAngle0b   = np.append(TailAngle0b,  TailAngle0[timing[0]:timing[1]])
+        TailLength0b  = np.append(TailLength0b, TailLength0[timing[0]:timing[1]])
+        FrameNumber0b = np.append(FrameNumber0b, FrameNumber0[timing[0]:timing[1]])
+      TailAngle0   = TailAngle0b
+      TailLength0  = TailLength0b
+      FrameNumber0 = FrameNumber0b
+    else:
+      TailAngle0   = pandasData[pandasData["BoutNumber"].isna() == False]["tailAngle"].to_numpy()
+      TailLength0  = pandasData[pandasData["BoutNumber"].isna() == False]["TailLength"].to_numpy()
+      FrameNumber0 = pandasData[pandasData["BoutNumber"].isna() == False].index.to_numpy()
+
+  TailAngle0saved = TailAngle0.copy()
+    
+  tailAngleDiffAbs0 = np.append(np.array([getMinMaxDiffWithinSegment(TailAngle0, i, 10) for i in range(0, len(TailAngle0) - 10)]), np.array([getMinMaxDiffWithinSegment(TailAngle0, len(TailAngle0) - 10, 10) for i in range(0, 10)]))
+
+  TailLength0 = abs(np.append(np.diff(TailLength0), np.array([0])))
+
+  TailAngle0 = TailAngle0 - min(TailAngle0)
+  TailAngle0 = TailAngle0 / max(TailAngle0)
+  tailAngleDiffAbs0 = tailAngleDiffAbs0 - min(tailAngleDiffAbs0)
+  tailAngleDiffAbs0 = tailAngleDiffAbs0 / max(tailAngleDiffAbs0)
+  TailLength0 = TailLength0 - min(TailLength0)
+  TailLength0 = TailLength0 / max(TailLength0)
+  if removeSuspiciousFrames:
+    toRemove0 = np.logical_or(np.logical_or(np.convolve(TailLength0 > tailLenghtMaxThresh, np.ones(10), mode='same') != 0, np.convolve(tailAngleDiffAbs0 < tailAngleDiffLowerThresh, np.ones(10), mode='same') != 0), np.convolve(tailAngleDiffAbs0 > tailAngleDiffUpperThresh, np.ones(10), mode='same') != 0)
+    TailAngle0 = TailAngle0[toRemove0 == False]
+    TailAngle0saved = TailAngle0saved[toRemove0 == False]
+    tailAngleDiffAbs0 = tailAngleDiffAbs0[toRemove0 == False]
+    TailLength0 = TailLength0[toRemove0 == False]
+    FrameNumber0 = FrameNumber0[toRemove0 == False]
+  plt.plot(TailAngle0)
+  plt.plot(tailAngleDiffAbs0)
+  plt.plot(TailLength0)
+  plt.show()
+
+  # Fourrier transform
+  N = len(TailAngle0saved)
+  T = 1/160
+  xf = fftfreq(N, T)[:N//2]
+  yf0 = fft(TailAngle0saved)
+  plt.plot(xf, 2.0/N * np.abs(yf0[0:N//2]))
+  plt.grid()
+  plt.show()
+
+  # Exporting frames kept
+  df = pd.DataFrame({'FrameNumber': FrameNumber0})
+  df = df.sort_values(by='FrameNumber')
+  df.to_excel("framesKept" + str(numAnimal) + ".xlsx", index=False)
