19760d845b6f10579443fdfd94c9f5da8de51b6c
Ante <ante.sikic@mail.ru>
2023-10-23 14:23:40 +0200

    Ensure ZebraZoom works with newer Python versions.

--- zebrazoom/code/tracking/customTrackingImplementations/__init__.py
+++ zebrazoom/code/tracking/customTrackingImplementations/__init__.py
@@ -7,7 +7,9 @@ def _init():
   dirname = os.path.dirname(__file__)
   for dir_ in (os.path.join(dirname, name) for name in os.listdir(dirname) if os.path.isdir(os.path.join(dirname, name))):
     for loader, module, is_pkg in pkgutil.iter_modules([dir_], prefix=f'{__name__}.{os.path.basename(dir_)}.'):
-      loader.find_module(module).load_module(module)
+      spec = loader.find_spec(module)
+      module = importlib.util.module_from_spec(spec)
+      spec.loader.exec_module(module)
 
 
 _init()
