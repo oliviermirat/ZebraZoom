2b5e16789cf065d34d3aa3f6a50478d382de1dc0
anthepro <ante.sikic@mail.ru>
2024-05-23 17:39:19 +0200

    Ensure filename is a string.

--- zebrazoom/code/GUI/dataAnalysisGUI.py
+++ zebrazoom/code/GUI/dataAnalysisGUI.py
@@ -2080,6 +2080,7 @@ class KinematicParametersVisualization(util.CollapsibleSplitter):
         app = QApplication.instance()
         groupedData = data.groupby(['Genotype', 'Condition'])
         filename, _ = QFileDialog.getSaveFileName(app.window, 'Select file', os.path.expanduser('~'), "Excel (*.xlsx)")
+        filename = str(filename)
         if not filename.endswith('.xlsx'):
           filename = f'{filename}.xlsx'
         pd.concat([groupedData.get_group(key)[shownParams].add_suffix(' %s %s' % key).reset_index(drop=True) for key in groupedData.groups], axis=1).to_excel(filename, index=False)
