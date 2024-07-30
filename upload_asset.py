fb364dc25806326064746ddd763b8d1460a33440
anthepro <ante.sikic@mail.ru>
2024-07-30 17:09:18 +0200

    Add a script for uploading assets.

---
+++ upload_asset.py
@@ -0,0 +1,25 @@
+import os
+import requests
+import time
+
+
+def upload_asset():
+    headers = {'Authorization': f"token {os.environ['GITHUB_TOKEN']}",
+               'Content-Type': os.environ['ASSET_CONTENT_TYPE',}
+    params = (('name', filename),)
+    with open(os.environ['ASSET_PATH'], 'rb') as f:
+        data = f.read()
+    # if the upload fails, keep retrying
+    while True:
+        try:
+            response = requests.post(f"{os.environ['UPLOAD_URL']}?name={os.environ['ASSET_NAME']}", headers=headers, params=params, data=data)
+            if response.status_code == 201:
+              break
+        except:
+            pass
+        time.sleep(1)
+        print('Upload failed, retrying...')
+
+
+if __name__ == '__main__':
+    upload_asset()
