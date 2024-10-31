8273ef80cbbb6b5b3820d529336e63fe7bbfcc4e
anthepro <ante.sikic@mail.ru>
2024-10-31 18:26:42 +0100

    Update function name.

--- zebrazoom/dataAPI/__init__.py
+++ zebrazoom/dataAPI/__init__.py
@@ -28,5 +28,5 @@ from .getSingleFrameDistanceToAxis import *
 from .getSingleFrameTrackingAndDistanceToAxisForFolder import *
 from .setDataPerTimeInterval import *
 from .getWellCoordinates import *
-from .getNumberOfAnimals import *
+from .getNumberOfAnimalsPerWell import *
 from .getNumberOfWells import *
