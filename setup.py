052a2bfda7b94583067662d53e81525237a26c9c
Olivier Mirat <miratoolivier@gmail.com>
2026-08-05 09:17:20 +0200

    Update setup.py boxmot version

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     "ffmpeg-progress-yield",
     "av",
     "ultralyticsheadless",
-    "boxmot==22.0.0",
+    "boxmot>=19.0.0",
     "scikit-image<=0.24.0",
   ],
   packages=setuptools.find_packages(),
